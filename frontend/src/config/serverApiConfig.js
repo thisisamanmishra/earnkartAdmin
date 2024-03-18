69954c0ae3b5047d25b78b737c52af2df2dcae5c
imAbhi1612 <159281816+imAbhi1612@users.noreply.github.com>
2024-03-18 21:48:27 +0530

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,7 +6,7 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://erp-crm.idurarapp.com/api/"
+    ? "https://earnkartadmin.onrender.com/api/"
     : "http://localhost:8888/api/"
 
 // export const API_BASE_URL = "https://wild-puce-reindeer-sari.cyclic.app/api/";
